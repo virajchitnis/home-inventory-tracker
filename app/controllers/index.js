c0a868da7f016150ccdc5d591acf07655b3b957c
Viraj Chitnis <chitnisviraj@gmail.com>
2020-03-28 14:30:30 +0000

    New items can be added

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,6 +6,15 @@ export default class IndexController extends Controller {
     @tracked
     searchString = '';
 
+    @tracked
+    newItemName = '';
+    @tracked
+    newItemOquant = 0;
+    @tracked
+    newItemNquant = 0;
+    @tracked
+    newItemExpDate = '';
+
     get inventoryItems() {
         let filteredItems = this.model.items.filter(({name}) => name.toLowerCase().includes(this.searchString.toLowerCase()));
         return filteredItems;
@@ -16,6 +25,51 @@ export default class IndexController extends Controller {
     }
 
     set searchText(value) {
+        this.newItemName = value;
         this.searchString = value;
+    }
+
+    get newName() {
+        return this.newItemName;
+    }
+
+    set newName(value) {
+        this.newItemName = value;
+    }
+
+    get newOquant() {
+        return this.newItemOquant;
+    }
+
+    set newOquant(value) {
+        this.newItemOquant = value;
+    }
+
+    get newNquant() {
+        return this.newItemNquant;
+    }
+
+    set newNquant(value) {
+        this.newItemNquant = value;
+    }
+
+    get newExpDate() {
+        return this.newItemExpDate;
+    }
+
+    set newExpDate(value) {
+        this.newItemExpDate = value;
+    }
+
+    @action
+    createNewItem() {
+        let newItem = {
+            name: this.newItemName,
+            oquant: this.newItemOquant,
+            nquant: this.newItemNquant,
+            exp_date: this.newItemExpDate
+        };
+        this.model.items.push(newItem);
+        this.searchString = '';
     }
 }
